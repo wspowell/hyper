acf6b75d729cfea4293f5b224b818bf15fa55273
Wesley Powell <wspowell@utexas.edu>
2015-03-16 16:55:27 -0500

    Added ServerState trait to Server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -18,6 +18,8 @@ use header::ConnectionOption::{Close, KeepAlive};
 use net::{NetworkListener, NetworkStream, HttpListener};
 use version::HttpVersion::{Http10, Http11};
 
+use std::sync::{Arc, RwLock};
+
 use self::listener::ListenerPool;
 
 pub mod request;
@@ -29,10 +31,11 @@ mod listener;
 ///
 /// Once listening, it will create a `Request`/`Response` pair for each
 /// incoming connection, and hand them to the provided handler.
-pub struct Server<'a, H: Handler, L = HttpListener> {
+pub struct Server<'a, H: Handler<C>, C: ServerState, L = HttpListener> {
     handler: H,
     ssl: Option<(&'a Path, &'a Path)>,
-    _marker: PhantomData<L>
+    _marker: PhantomData<L>,
+	state: Arc<RwLock<C>>
 }
 
 macro_rules! try_option(
@@ -44,33 +47,37 @@ macro_rules! try_option(
     }}
 );
 
-impl<'a, H: Handler, L: NetworkListener> Server<'a, H, L> {
+impl<'a, H: Handler<C>, C: ServerState, L: NetworkListener> Server<'a, H, C, L> {
     /// Creates a new server with the provided handler.
-    pub fn new(handler: H) -> Server<'a, H, L> {
+    pub fn new(handler: H, mut state: C) -> Server<'a, H, C, L> {
+		state.reset();
         Server {
             handler: handler,
             ssl: None,
-            _marker: PhantomData
+            _marker: PhantomData,
+			state: Arc::new(RwLock::new(state))
         }
     }
 }
 
-impl<'a, H: Handler + 'static> Server<'a, H, HttpListener> {
+impl<'a, H: Handler<C> + 'static, C: ServerState> Server<'a, H, C, HttpListener> {
     /// Creates a new server that will handle `HttpStream`s.
-    pub fn http(handler: H) -> Server<'a, H, HttpListener> {
-        Server::new(handler)
+    pub fn http(handler: H, state: C) -> Server<'a, H, C, HttpListener> {
+        Server::new(handler, state)
     }
     /// Creates a new server that will handler `HttpStreams`s using a TLS connection.
-    pub fn https(handler: H, cert: &'a Path, key: &'a Path) -> Server<'a, H, HttpListener> {
+    pub fn https(handler: H, mut state: C, cert: &'a Path, key: &'a Path) -> Server<'a, H, C, HttpListener> {
+		state.reset();
         Server {
             handler: handler,
             ssl: Some((cert, key)),
-            _marker: PhantomData
+            _marker: PhantomData,
+			state: Arc::new(RwLock::new(state))
         }
     }
 }
 
-impl<'a, H: Handler + 'static> Server<'a, H, HttpListener> {
+impl<'a, H: Handler<C> + 'static, C: ServerState + 'static> Server<'a, H, C, HttpListener> {
     /// Binds to a socket, and starts handling connections using a task pool.
     pub fn listen_threads(self, ip: IpAddr, port: u16, threads: usize) -> HttpResult<Listening> {
         let addr = &(ip, port);
@@ -88,18 +95,19 @@ impl<'a, H: Handler + 'static> Server<'a, H, HttpListener> {
 }
 impl<
 'a,
-H: Handler + 'static,
+H: Handler<C> + 'static,
+C: ServerState + 'static,
 L: NetworkListener<Stream=S> + Send + 'static,
-S: NetworkStream + Clone + Send> Server<'a, H, L> {
+S: NetworkStream + Clone + Send> Server<'a, H, C, L> {
     /// Creates a new server that will handle `HttpStream`s.
     pub fn with_listener(self, mut listener: L, threads: usize) -> HttpResult<Listening> {
         let socket = try!(listener.socket_addr());
         let handler = self.handler;
+		let state = self.state;
 
         debug!("threads = {:?}", threads);
         let pool = ListenerPool::new(listener.clone());
-        let work = move |stream| keep_alive_loop(stream, &handler);
-
+        let work = move |stream| keep_alive_loop(stream, &handler, &state);
         let guard = thread::scoped(move || pool.accept(work, threads));
 
         Ok(Listening {
@@ -109,9 +117,8 @@ S: NetworkStream + Clone + Send> Server<'a, H, L> {
     }
 }
 
-
-fn keep_alive_loop<'h, S, H>(mut stream: S, handler: &'h H)
-where S: NetworkStream + Clone, H: Handler {
+fn keep_alive_loop<'h, S, C, H>(mut stream: S, handler: &'h H, state: &Arc<RwLock<C>>)
+where S: NetworkStream + Clone, H: Handler<C>, C: ServerState {
     debug!("Incoming stream");
     let addr = match stream.peer_addr() {
         Ok(addr) => addr,
@@ -127,17 +134,19 @@ where S: NetworkStream + Clone, H: Handler {
 
     let mut keep_alive = true;
     while keep_alive {
-        keep_alive = handle_connection(addr, &mut rdr, &mut wrt, handler);
+		let local_state = state.clone();
+        keep_alive = handle_connection(addr, &mut rdr, &mut wrt, handler, local_state);
         debug!("keep_alive = {:?}", keep_alive);
     }
 }
 
-fn handle_connection<'a, 'aa, 'h, S, H>(
+fn handle_connection<'a, 'aa, 'h, S, H, C>(
     addr: SocketAddr,
     rdr: &'a mut BufReader<&'aa mut NetworkStream>,
     wrt: &mut BufWriter<S>,
-    handler: &'h H
-) -> bool where 'aa: 'a, S: NetworkStream, H: Handler {
+    handler: &'h H,
+	local_state: Arc<RwLock<C>>
+) -> bool where 'aa: 'a, S: NetworkStream, H: Handler<C>, C: ServerState {
     let mut res = Response::new(wrt);
     let req = match Request::<'a, 'aa>::new(rdr, addr) {
         Ok(req) => req,
@@ -158,7 +167,7 @@ fn handle_connection<'a, 'aa, 'h, S, H>(
         _ => true
     };
     res.version = req.version;
-    handler.handle(req, res);
+    handler.handle(req, res, local_state);
     keep_alive
 }
 
@@ -178,16 +187,34 @@ impl Listening {
     }
 }
 
+/// A server state that is passed to the handling function.
+/// The server state holds server information that is use for every request.
+///
+/// Meant to be used as a server cache that is only sometimes updated and can be used 
+/// to store data such as server configuration, basic static sql query results, etc.
+pub trait ServerState: Sync + Send {
+	/// Reset the server state to a default state.
+	/// Called on server start up and if a server state write operation becomes poisoned.
+	fn reset(&mut self);
+}
+
 /// A handler that can handle incoming requests for a server.
-pub trait Handler: Sync + Send {
+pub trait Handler<C: ServerState>: Sync + Send {
     /// Receives a `Request`/`Response` pair, and should perform some action on them.
     ///
     /// This could reading from the request, and writing to the response.
-    fn handle<'a, 'aa, 'b, 's>(&'s self, Request<'aa, 'a>, Response<'b, Fresh>);
+    fn handle<'a, 'aa, 'b, 's>(&'s self, Request<'aa, 'a>, Response<'b, Fresh>, Arc<RwLock<C>>);
 }
 
-impl<F> Handler for F where F: Fn(Request, Response<Fresh>), F: Sync + Send {
-    fn handle<'a, 'aa, 'b, 's>(&'s self, req: Request<'a, 'aa>, res: Response<'b, Fresh>) {
-        self(req, res)
+impl<C: ServerState, F> Handler<C> for F where F: Fn(Request, Response<Fresh>, Arc<RwLock<C>>), F: Sync + Send {
+    fn handle<'a, 'aa, 'b, 's>(&'s self, req: Request<'a, 'aa>, res: Response<'b, Fresh>, state: Arc<RwLock<C>>) {
+        self(req, res, state)
     }
 }
+
+
+
+
+
+
+
